1b1ce3ba9415105c209defd0677762d896a83150
The Customer <niangalytall@gmail.com>
2022-07-13 11:52:38 +0000

    2ie commic : image et missile

--- script.js
+++ script.js
@@ -28,31 +28,88 @@ class Player{
         this.position = {
             x: (space.width - this.width)/2,
             y: space.height - this.height
-        };   
+        };  
+        //Creation de l'image du joueur
+        const avatar = new Image(); 
+        avatar.src = 'avion.png';
+        avatar.onload = ()=>{// attendre que le chargement de l'image
+            //Creation de l'attribut image pour la classe Player
+            this.image = avatar;
+            //Prendre la taille de l'image comme taille du joueur
+            this.width =  this.image.width * 1;
+            this.height = this.image.height * 1;
+            //Faire remonter un peu le joueur
+            this.position.y -= 30
+        }
     }
+    
     //Methode pour dessiner le joueur
     print(ctx){
         ctx.fillStyle = 'white'; //Couleur du dessin
         ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
     }
-    //Mettre a jour le joueur : Redessiner
+    //Methode pour afficher l'image du joueur
+    printImage(ctx){
+        ctx.drawImage(this.image,this.position.x,this.position.y,this.width,this.height);
+    }
+
+    //Mettre a jour la positio du joueur
     updatePosition(){
-        if(controllers.left.pressed && this.position.x >=0){
-            //deplacer le joueur vers la gauche
-            this.velocity.x = -speed;
-        }else if(controllers.right.pressed && this.position.x <= space.width - this.width){
-            this.velocity.x = speed;
-        }else{
-            this.velocity.x = 0;
+        if(this.image){ // Verifier si l'image est deja chargée
+            if(controllers.left.pressed && this.position.x >=0){
+                //deplacer le joueur vers la gauche
+                this.velocity.x = -speed;
+            }else if(controllers.right.pressed && this.position.x <= space.width - this.width){
+                this.velocity.x = speed;
+            }else{
+                this.velocity.x = 0;
+            }
+            //Repositionner le joueur
+            this.position.x += this.velocity.x;
+            //Mettre a jour le joueur : Redessiner (sans image)
+            // this.print(ctx);
+            //Mettre a jour le joueur : Redessiner (avec image)
+            this.printImage(ctx);
         }
-        //Repositionner le joueur
-        this.position.x += this.velocity.x;
+    }
+
+    //Tirer une missile
+    fire(){
+        //1. Creer une missile
+        shots.push(new Missile({
+            position:{
+                x : this.position.x + this.width/2, 
+                y :this.position.y
+            }
+        }));
+    }
+}
+
+//Classe Missile
+class Missile{
+    constructor({position}){
+        this.position = position;
+        this.velocity = -(speed * 3)/2;
+        this.width = 3;
+        this.height =10;
+    }
+
+    print(ctx){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
+        ctx.fillRect(this.position.x-10,this.position.y-10,this.width,this.height);
+        ctx.fillRect(this.position.x-20,this.position.y,this.width,this.height);
+    }
+
+    updatePosition(){
+        this.position.y += this.velocity;
         this.print(ctx);
     }
 }
 
 //====================Lancement du jeu===============
 const player = new Player();
+
 
 function startGame() {
      //Pour faire une boucle infinie sur l'animation
@@ -61,6 +118,14 @@ function startGame() {
     //Effacer l'ancien contenu
     ctx.clearRect(0,0,space.width,space.height);
     player.updatePosition();
+    //afficher les missiles du tableau shots
+    shots.forEach((missile,i)=>{
+        if(missile.position.y + missile.height <=0){ // si la missile sort de l'espace de jeu
+            shots.splice(i,1); // on supprime la missile du tableau shots
+        }else{
+            missile.updatePosition();
+        }
+    });
 }
 
 
@@ -95,6 +160,8 @@ addEventListener('keyup',({key})=>{
             break;
         case ' ':
             //Feu!!!!!
+            player.fire();
+            console.log(shots);
             break;
     }
 });
